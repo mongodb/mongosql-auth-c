5e177cf94feddc97357d0c9424a7c444af27fbc7
Wisdom Omuya <deafgoat@gmail.com>
2017-12-06 14:52:57 -0500

    BI-1098: use latest green bic build for tests

--- test/bin/download-sqlproxy.py
+++ test/bin/download-sqlproxy.py
@@ -1,29 +1,30 @@
 """
-This script automates the process of downloding the most recent sqlproxy releases
+This script automates the process of downloading the latest BI Connector nigthly.
 """
 
-import ast
-import datetime
-import getopt
-import json
 import os
-import re
-import subprocess
 import sys
-import tempfile
 import urllib2
 
-import requests
 
-EVG_BASE = "https://evergreen.mongodb.com/rest/v1/"
-UNITS = ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']
-USAGE = """
-SQLProxy Download tool 0.1
-Usage:
-  download-sqlproxy.py (-h | --help)
-Options:
-  -h --help     Show this screen.
-"""
+def download_binary(build_variant):
+    """Downloads a binary from Amazon's S3.
+    """
+    file_name = "sqlproxy"
+    extension = "msi" if "win" in build_variant else "tgz"
+    url = "https://s3.amazonaws.com/info-mongodb-com/mongodb-bi/v2/latest/mongodb-bi-%s-latest.%s" % (build_variant, extension)
+    print url
+    data = urllib2.urlopen(url)
+    file_path = open(os.path.join(os.environ.get('SQLPROXY_DOWNLOAD_DIR'), file_name), 'wb')
+    meta = data.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    block_sz = 8192
+    while True:
+        buf = data.read(block_sz)
+        if not buf:
+            break
+        file_path.write(buf)
+    file_path.close()
 
 
 def ensure_env():
@@ -49,129 +50,8 @@ def ensure_env():
         sys.exit(1)
 
 
-class SQLProxyDownloader(object):
-    """Represents a release instance.
-    """
-    def __init__(self, build_variant):
-        """Initialize a new SQLProxyDownloader object.
-        """
-        self.__urls = {}
-        self.__build_variant = build_variant
-
-    def download_binaries(self):
-        """Downloads the binaries from Evergreen.
-        """
-        for entry, url in self.__urls.items():
-            print(entry)
-
-            if entry != self.__build_variant:
-                continue
-
-            file_name = "sqlproxy"
-            data = urllib2.urlopen(url)
-            file_path = open(os.path.join(os.environ.get('SQLPROXY_DOWNLOAD_DIR'), file_name), 'wb')
-            meta = data.info()
-            file_size = int(meta.getheaders("Content-Length")[0])
-            block_sz = 8192
-            while True:
-                buf = data.read(block_sz)
-                if not buf:
-                    break
-                file_path.write(buf)
-            file_path.close()
-
-    def fetch_latest_version(self):
-        url = "%s/projects/sqlproxy/versions" % (EVG_BASE)
-        user_name = os.environ.get('EVG_USER', None)
-        evg_key = os.environ.get('EVG_KEY', None)
-        headers = {'Auth-Username': user_name, 'Api-Key': evg_key}
-        print("Contacting Evergreen at %s" % (url))
-        response = requests.get(url, headers=headers)
-        if response.status_code != 200:
-            print("Can't contact Evergreen at %s: %s" % (url, response.text))
-            sys.exit(1)
-        data = json.loads(response.text)
-        self.__version = data["versions"][0]["version_id"]
-        print("Using latest version '%s'") % (self.__version)
-
-    def fetch_urls(self):
-        """Fetches the release's binaries URL from the Evergreen API.
-        """
-        url = "%s/versions/%s" % (EVG_BASE, self.__version)
-        user_name = os.environ.get('EVG_USER', None)
-        evg_key = os.environ.get('EVG_KEY', None)
-        headers = {'Auth-Username': user_name, 'Api-Key': evg_key}
-        print("Contacting Evergreen at %s" % (url))
-        response = requests.get(url, headers=headers)
-        if response.status_code != 200:
-            print("Can't contact Evergreen at %s: %s" % (url, response.text))
-            sys.exit(1)
-        versions = json.loads(response.text)
-        builds = versions["builds"]
-
-        if len(builds) == 0:
-            print "No builds found for version '%s'" % (self.__version)
-            sys.exit(0)
-
-        for build_name in builds:
-            # don't upload binaries for race buildvariant
-            if "race" in build_name:
-                print("Skipping build %s..." % (build_name))
-                continue
-            url = "%s/builds/%s" % (EVG_BASE, build_name)
-            print("Fetching build %s..." % (url))
-            response = requests.get(url, headers=headers)
-            if response.status_code != 200:
-                print("Can't contact Evergreen")
-                response.raise_for_status()
-            build = json.loads(response.text)
-            task = build["tasks"]
-            sign = task.get("sign", None)
-
-            if sign is None:
-                print("No sign task found for '%s'" % (build["name"]))
-                continue
-
-            status = sign.get("status", None)
-
-            if status is None:
-                print("No status found for '%s'" % (build["name"]))
-                sys.exit(1)
-
-            if status != "success":
-                print("%s sign has status '%s', exiting..." % (build["name"], \
-                    status))
-                continue
-
-            url = "%s/tasks/%s" % (EVG_BASE, sign["task_id"])
-            print("Fetching task %s..." % (url))
-            response = requests.get(url, headers=headers)
-
-            if response.status_code != 200:
-                print("Can't contact Evergreen")
-                sys.exit(1)
-
-            entry = json.loads(response.text)
-            variant = entry["build_variant"]
-            extension = ".msi" if "windows" in variant else ".tgz"
-            for file in entry["files"]:
-                url = file["url"]
-                _, ext = os.path.splitext(url)
-                if ext == extension:
-                    self.__urls[variant] = url
-                    break
-
-    def run(self):
-        """Runs an instance of the SQLProxy Downloader.
-        """
-        self.fetch_latest_version()
-        self.fetch_urls()
-        self.download_binaries()
-
 if __name__ == '__main__':
     ensure_env()
     build_variant = os.getenv('SQLPROXY_BUILD_VARIANT')
     print("downloading build variant '%s'" % (build_variant))
-    releaser = SQLProxyDownloader(build_variant)
-    releaser.run()
-
+    download_binary(build_variant)
--- test/bin/platforms.sh
+++ test/bin/platforms.sh
@@ -2,26 +2,26 @@
 case $VARIANT in
 ubuntu1404-64)
     export CC='/opt/mongodbtoolchain/v2/bin/clang'
-    export SQLPROXY_BUILD_VARIANT='full_matrix__os_full_matrix~ubuntu1404-64_mongodb_version~latest_mongodb_topology~standalone'
+    export SQLPROXY_BUILD_VARIANT='linux-x86_64-ubuntu-1404'
     CMAKE_PATH='/opt/cmake/bin'
     PUSH_ARCH='x86_64-ubuntu-1404'
     PUSH_NAME='linux'
     ;;
 rhel70)
     export CC='/opt/mongodbtoolchain/v2/bin/clang'
-    export SQLPROXY_BUILD_VARIANT='single_variant__os_single_variant~rhel70'
+    export SQLPROXY_BUILD_VARIANT='linux-x86_64-rhel70'
     CMAKE_PATH='/opt/cmake/bin'
     PUSH_ARCH='x86_64-rhel70'
     PUSH_NAME='linux'
     ;;
 osx)
-    export SQLPROXY_BUILD_VARIANT='full_matrix__os_full_matrix~osx_mongodb_version~latest_mongodb_topology~standalone'
+    export SQLPROXY_BUILD_VARIANT='osx-x86_64'
     CMAKE_PATH='/Applications/Cmake.app/Contents/bin'
     PUSH_ARCH='x86_64'
     PUSH_NAME='osx'
     ;;
 windows-vs2013)
-    export SQLPROXY_BUILD_VARIANT='full_matrix__os_full_matrix~windows_mongodb_version~latest_mongodb_topology~standalone'
+    export SQLPROXY_BUILD_VARIANT='win32-x86_64'
     CMAKE_PATH='/cygdrive/c/cmake/bin'
     CMAKE_GENERATOR='Visual Studio 12 2013 Win64'
     MSBUILD_PATH='/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin'
@@ -30,7 +30,7 @@ windows-vs2013)
     PUSH_NAME='win32'
     ;;
 windows-32)
-    export SQLPROXY_BUILD_VARIANT='full_matrix__os_full_matrix~windows_mongodb_version~latest_mongodb_topology~standalone'
+    export SQLPROXY_BUILD_VARIANT='win32-x86_64'
     CMAKE_PATH='/cygdrive/c/cmake/bin'
     CMAKE_GENERATOR='Visual Studio 12 2013'
     MSBUILD_PATH='/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin'
