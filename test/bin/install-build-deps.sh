2830bc12994dfae488ac7375ca7d15c1e286f847
Wisdom Omuya <deafgoat@gmail.com>
2018-02-22 17:02:39 -0500

    BI-1490: Upload system bison to s3

--- test/bin/install-build-deps.sh
+++ test/bin/install-build-deps.sh
@@ -9,7 +9,7 @@
         echo "installing build dependencies..."
         mkdir $ARTIFACTS_DIR/bison
         cd $ARTIFACTS_DIR/bison
-        curl -O https://superb-sea2.dl.sourceforge.net/project/gnuwin32/bison/2.4.1/bison-2.4.1-setup.exe
+        curl -O https://noexpire.s3.amazonaws.com/sqlproxy/binary/bison-2.4.1-setup.exe
         chmod +x bison-2.4.1-setup.exe
         ./bison-2.4.1-setup.exe /VERYSILENT /DIR='C:\bison'
         echo "done installing build dependencies"
